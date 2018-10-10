677cf015b6e5a11a5132fd1824007cd8a96fcee3
Daniel Soskel <unstablereality@gmail.com>
2018-10-10 14:30:25 -0500

    Browser selection works. Randomly selects a browser from mobile, mac, and windows categories. Currently outputs to command line for testing purposes.

--- cbt_test.js
+++ cbt_test.js
@@ -2,28 +2,47 @@ var cbt = require('cbt_tunnels');
 var request = require('request');
 var APIUrl = 'https://daniel.soskel@gmail.com:ua01f835227df050@crossbrowsertesting.com/api/v3';
 
-// cbt.start({
-//     username: "daniel.soskel@gmail.com",
-//     authkey: "ua01f835227df050",
-//     verbose: true,
-//     kill: "stopserver",
-//     dir: "C:\\Users\\Daniel\\WebstormProjects\\CBT_Test\\"},
-//     function(err) {
-//         if (!err) console.log(getBrowsers());
-//     }
-// );
-
-console.log(getBrowsers());
+cbt.start(
+    {
+    username: "daniel.soskel@gmail.com",
+    authkey: "ua01f835227df050",
+    kill: "stopserver",
+    dir: "C:\\Users\\DSOSKEL\\WebstormProjects\\CBT_Test\\"},
+    function(err) {
+        if (!err) {
+            getBrowsers();
+        }
+    },
+    // fs.open('stopserver', 'w', function(err) { if (err) throw err })
+);
 
 function getBrowsers() {
     request(APIUrl + '/selenium/browsers', {json: true}, (err, res, body) => {
-        if (err) {return console.log(err)}
-        return filterBrowsers(body, "mobile");
+        if (err) {return console.log('Error: ' + err)}
+        var mobile = getRandom(filterByField(body, 'device', 'mobile'));
+        var windows = getRandom(getDesktops(body, 'Windows'));
+        var mac = getRandom(getDesktops(body, 'Mac'));
+        console.log(mobile.api_name);
+        console.log(windows.api_name);
+        console.log(mac.api_name);
     });
 }
 
-function filterBrowsers(arrBrowsers, device) {
-    arrBrowsers.filter(function(browser) {
-        return browser.device == device;
-    })
+function filterByField(arrDevices, field, filter) {
+    // filtered = arrDevices.filter(({device}) => device === filter);
+    return arrDevices.filter( browser => {
+        return browser[field] === filter;
+    });
+}
+
+function getDesktops(arrDevices, type) {
+    var desktops = filterByField(arrDevices, 'device', 'desktop');
+    return filterByField(desktops, 'type', type);
+}
+
+function getRandom(array) {
+    var arrKeys = Object.keys(array);
+    var randKey = arrKeys[Math.floor(Math.random() * arrKeys.length)];
+    return array[randKey];
+
 }
