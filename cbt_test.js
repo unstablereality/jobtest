6f5109bef2d643a9f77794d2553bc944fc0f5935
Daniel Soskel <unstablereality@gmail.com>
2018-10-11 14:48:22 -0500

    Gave up on Async for now, working on getting the selenium tests to process.

--- cbt_test.js
+++ cbt_test.js
@@ -1,10 +1,57 @@
 var cbt = require('cbt_tunnels');
 var fs = require('fs');
 var request = require('request');
+var util = require ('util');
 var selenium = require('./selenium.js');
-var util = require('util');
-var rqAsync = util.promisify(request);
 var APIUrl = 'https://daniel.soskel@gmail.com:ua01f835227df050@crossbrowsertesting.com/api/v3';
+
+var mobileDevice = { api_name: 'iPadAir-iOS8Sim',
+    device: 'mobile',
+    device_type: 'tablet',
+    name: 'iPad Air / 8.1 Simulator',
+    version: '8.1',
+    type: 'iPad',
+    icon_class: 'ipad-landscape',
+    upload_file_enabled: true,
+    sort_order: 1.507,
+    is_webrtc_enabled: false,
+    caps:
+        { deviceName: 'iPad Air Simulator',
+            platformName: 'iOS',
+            platformVersion: '8.1' },
+    browsers:
+        [ { name: 'Mobile Safari 8.0',
+            type: 'Mobile Safari',
+            version: '8.0',
+            api_name: 'MblSafari8.0',
+            default_live_test_browser: true,
+            icon_class: 'safari-mobile',
+            major_browser: true,
+            device: 'mobile',
+            selenium_version: '3.4.0',
+            webdriver_type: 'appium',
+            webdriver_version: '1.3.4',
+            can_mobile_debug: false,
+            caps: { browserName: 'Safari' },
+            default_config: 'iPadAir-iOS8Sim' } ],
+    resolutions:
+        [ { width: 1536,
+            height: 2048,
+            name: '1536x2048',
+            desktop_width: 1600,
+            desktop_height: 1200,
+            default: false,
+            orientation: 'portrait',
+            caps: { deviceOrientation: 'portrait' } },
+            { width: 2048,
+                height: 1536,
+                name: '2048x1536',
+                desktop_width: 1600,
+                desktop_height: 1200,
+                default: true,
+                orientation: 'landscape',
+                caps: { deviceOrientation: 'landscape' } } ]
+};
 
 cbt.start(
     {
@@ -15,34 +62,34 @@ cbt.start(
     },
     function (err) {
         if (!err) {
-            var devices = ['mobile'];
-            devices.forEach(function (device) {
-                var target = getDevice(device);
-                target.then(function(result) {
-                    console.log("Result :" + result)
-                })
-            });
+            // var devices = ['mobile'];
+            // devices.forEach(function (device) {
+            //     getDevice(device);
+            // });
+            browser = mobileDevice.browsers[0];
+            resolution = mobileDevice.resolutions[0];
+            console.log('browserName: ' + browser.caps.browserName + ' version: ' + browser.version + ' platform: ' + mobileDevice.caps.platformName + ' screenResolution: ' + resolution.name);
+            //selenium.runTest(mobileDevice, browser, resolution);
         }
     },
-    fs.open('stopserver', 'w', function (err) {
-        if (err) throw err
-    })
+    // fs.open('stopserver', 'w', function (err) {
+    //     if (err) throw err
+    // })
 );
 
 async function getDevice(deviceType) {
-    var response = await rqAsync(APIUrl + '/selenium/browsers', {json: true}, (err, res, body) => {
+    request(APIUrl + '/selenium/browsers', {json: true}, (err, res, body) => {
         if (err) {
             return console.log('Error: ' + err)
         }
-        return body
+        if (deviceType === 'mobile') {
+            target = getRandom(filterByField(body, 'device', deviceType));
+            console.log(util.inspect(target, {showHidden: false, depth: null}));
+        }
+        else {
+            return getRandom(getDesktops(body, deviceType))
+        }
     });
-
-    if (deviceType === 'mobile') {
-        return getRandom(filterByField(response, 'device', deviceType));
-    }
-    else {
-        return getRandom(getDesktops(response, deviceType))
-    }
 }
 
 function filterByField(arrDevices, field, filter) {
--- selenium.js
+++ selenium.js
@@ -1,49 +1,102 @@
 "use strict";
 
-exports.runTest = function () {
-    var webdriver = require("selenium-webdriver"),
-        SeleniumServer = require("selenium-webdriver/remote").SeleniumServer;
-
-    var cbtHub = "http://hub.crossbrowsertesting.com:80/wd/hub";
+exports.runTest = function (device, browser, resolution) {
 
     var username = 'daniel.soskel@gmail.com';
     var authkey = 'ua01f835227df050';
 
-    var caps = {
-        'name': 'Page Title Test',
-        'build': '1.0',
-        'browserName': 'Internet Explorer',
-        'version': '10',
-        'platform': 'Windows 7 64-Bit',
-        'screenResolution': '1366x768'
-    };
+    var webdriver = require('selenium-webdriver'),
+        SeleniumServer = require('selenium-webdriver/remote').SeleniumServer,
+        request = require('request');
 
-    caps.username = username;
-    caps.password = authkey;
+    var remoteHub = "http://hub.crossbrowsertesting.com:80/wd/hub";
 
-    var driver = new webdriver.Builder()
-        .usingServer(cbtHub)
-        .withCapabilities(caps)
-        .build();
+    var browsers = [
+        { browserName: 'Firefox', platform: 'Windows 7 64-bit', version: '27', screen_resolution: '1024x768' },
+        { browserName: 'Chrome', platform: 'Mac OSX 10.9', version: '40x64', screen_resolution: '1024x768' },
+        { browserName: 'Internet Explorer', platform: 'Windows 8.1', version: '11', screen_resolution: '1024x768' }
+    ];
 
-    function checkTitle() {
-        driver.getTitle()
-            .then(function (title) {
-                console.log("The title is: " + title)
+    var flows = browsers.map(function(browser) {
+
+        var caps = {
+            name : 'Node Parallel Example',
+            browserName : browser.browserName,
+            version : browser.version,
+            platform : browser.platform,
+            screen_resolution : browser.screen_resolution,
+            username : username,
+            password : authkey
+        };
+
+        caps.username = username;
+        caps.password = authkey;
+
+        var driver = new webdriver.Builder()
+            .usingServer(remoteHub)
+            .withCapabilities(caps)
+            .build();
+
+        driver.getSession().then(function(session){
+
+            var sessionId = session.id_;
+
+            driver.get('http://local/TestPage.html');
+            var element = driver.findElement(webdriver.By.name('q'));
+            element.sendKeys('cross browser testing');
+            element.submit();
+            driver.getTitle().then(function(title) {
+                if (title !== ('Take Home Test')) {
+                    throw Error('Unexpected title: ' + title);
+                }
             });
-        return webdriver.until.titleIs('Take Home Test');
-    }
-
-    function handleFailure(err) {
-        console.error('Something went wrong!\n', err.stack, '\n');
-        quitDriver();
-    }
-
-    function quitDriver() {
-        console.log("WebDriver is about to close.");
-        driver.quit();
-    }
-
-    driver.get('http://local/TestPage.html');
-    return driver.wait(checkTitle, 1000).then(quitDriver, handleFailure);
+            driver.quit();
+        });
+    });
+    // var webdriver = require("selenium-webdriver"),
+    //     SeleniumServer = require("selenium-webdriver/remote").SeleniumServer;
+    //
+    // var cbtHub = "http://hub.crossbrowsertesting.com:80/wd/hub";
+    //
+    // var username = 'daniel.soskel@gmail.com';
+    // var authkey = 'ua01f835227df050';
+    //
+    // var caps = {
+    //     'name': 'Page Title Test',
+    //     'build': '1.0',
+    //     'browserName': browser.caps.browserName,
+    //     'deviceName': device.caps.device,
+    //     'platformVersion': device.caps.platformVersion,
+    //     'platformName': device.caps.platformName,
+    //     'deviceOrientation': resolution.deviceOrientation
+    // };
+    //
+    // caps.username = username;
+    // caps.password = authkey;
+    //
+    // var driver = new webdriver.Builder()
+    //     .usingServer(cbtHub)
+    //     .withCapabilities(caps)
+    //     .build();
+    //
+    // function checkTitle() {
+    //     driver.getTitle()
+    //         .then(function (title) {
+    //             console.log("The title is: " + title)
+    //         });
+    //     return webdriver.until.titleIs('Selenium Test Example Page');
+    // }
+    //
+    // function handleFailure(err) {
+    //     console.error('Something went wrong!\n', err.stack, '\n');
+    //     quitDriver();
+    // }
+    //
+    // function quitDriver() {
+    //     console.log("WebDriver is about to close.");
+    //     driver.quit();
+    // }
+    //
+    // driver.get('http://crossbrowsertesting.github.io/selenium_example_page.html');
+    // driver.wait(checkTitle, 1000).then(quitDriver, handleFailure);
 };
